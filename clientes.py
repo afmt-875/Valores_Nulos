b31e6305f404f60fb6128a50ccaf1d28e3bb1179
afmt-875 <a01704728@tec.mx>
2024-04-15 19:35:19 -0600

    Actualización

---
+++ clientes.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+
+DF = pd.read_excel('clientes.xlsx')
--- ventas.py
+++ ventas.py
@@ -14,4 +14,23 @@ df['tarjetas_debito'] = df['tarjetas_debito'].fillna(round(df['tarjetas_debito']
 
 #quitar nulos tarjeta de credito
 df['tarjetas_credito'] = df['tarjetas_credito'].fillna(round(df['tarjetas_credito'].median(),1))
-print(df.isnull().sum())
+#print(df.isnull().sum())
+
+#quitar nulos de otros medios
+df['otros_medios']=df['otros_medios'].fillna(6922148.759)
+#print(df.isnull().sum())
+
+# rellena valores nulos con metodo ffill
+df['subtotal_ventas_alimentos_bebidas'] =df['subtotal_ventas_alimentos_bebidas'].fillna(method='ffill')
+#print(df.isnull().sum())
+
+# rellena valores nulos con metodo bfill
+df['almacen'] =df['almacen'].fillna(method='bfill')
+#print(df.isnull().sum())
+
+# rellena con ceros la columna panaderia
+df['panaderia'] =df['panaderia'].fillna(0)
+#print(df.isnull().sum())
+
+#Convertir DataFrame a CSV
+df.to_csv('ventas_totales_limpio.csv')
